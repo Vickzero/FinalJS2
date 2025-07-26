949789708321473f78f3696a8767643e20c2a176
Vickzero <viebyield@hotmail.com>
2025-07-25 21:27:47 -0600

    se agrega fractional.js

--- src/js/views/recipeview.js
+++ src/js/views/recipeview.js
@@ -1,5 +1,7 @@
 import View from './view.js';
 import icons from 'url:../../img/icons.svg';
+import Fraction from 'fraction.js';
+
 
 class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -63,7 +65,7 @@ class RecipeView extends View {
                             <svg class="recipe__icon">
                                 <use xlink:href="${icons}#icon-check"></use>
                             </svg>
-                            <div class="recipe__quantity">${ing.quantity}</div>
+                            <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''}</div>
                             <div class="recipe__description">
                                 <span class="recipe__unit">${ing.unit}</span>
                                 ${ing.description}
